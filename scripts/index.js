91dbc402dda2b433788ab9e80d14c90471e8aa8a
Harry Tkachenko <tharry@yandex.ru>
2021-04-25 00:23:15 +0300

    основные правки. +JavaScript

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,12 @@
 let editProfileButton = document.querySelector('.profile__edit-button'); // присвоил кнопке Редактировать
 let popup = document.querySelector('.popup'); // присвоил весь контейнер
 let closeEditFormButton = document.querySelector('.popup__close'); // присвоил кнопке Закрыть
-let nameInput = popup.querySelector('.popup__name'); // строка "Имя" в Инпуте Попап.
-let jobInput = popup.querySelector('.popup__career'); // строка "О себе" в Инпуте Попап.
+let nameInput = popup.querySelector('.popup__input_name-js'); // строка "Имя" в Инпуте Попап.
+let jobInput = popup.querySelector('.popup__input_career-js'); // строка "О себе" в Инпуте Попап.
 let saveButton = popup.querySelector('.popup__save-button'); // кнопка Сохранить в попапе.
 let profileName = document.querySelector('.profile__name'); // строка "Имя" в ХТМЛ
 let profileCareer = document.querySelector('.profile__career'); // строка "О себе" в ХТМЛ
+let submitForm = document.querySelector('.popup__container');
 
 function openForm() {  // функция добавляет класс для показа попапа
     popup.classList.add('popup_is-opened');
@@ -24,23 +25,21 @@ function clearForm() {  // функция очистки инпутов попа
 
 function profileValueToForm () {  // функция которая позволяет забирать значения из HTML в Form.
     nameInput.value = profileName.textContent;
-    console.log(profileName.textContent);
     jobInput.value = profileCareer.textContent;
-    console.log(profileCareer.textContent);
 }
 
 function formSubmitHandler (evt) {  // функция которая позволяет забирать значения из Инпута в ХТМЛ.
     evt.preventDefault();
     profileName.textContent = nameInput.value;
+    console.log(profileName.textContent = nameInput.value);
     profileCareer.textContent = jobInput.value;
+    console.log(profileCareer.textContent = jobInput.value);
     closeForm();
 }
 
-
 editProfileButton.addEventListener('click', openForm);
 closeEditFormButton.addEventListener('click', closeForm);
-saveButton.addEventListener('click', formSubmitHandler);
-
+submitForm.addEventListener('submit', formSubmitHandler);
 
 
 
